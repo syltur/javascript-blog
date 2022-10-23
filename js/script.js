2ed5d4563963d97feee8b43d9bc14b1ff72d9db8
syltur <sylwia.tur99@gmail.com>
2022-10-23 16:47:12 +0200

    add generateTags function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-function generateTitleLinks() {
-  const ArticleSelector = ('.post');
-  const TitleSelector = ('.post-title');
-  const TitleListSelector =('.titles');
+{'use strict';
+  function generateTitleLinks() {
+  const ArticleSelector = ('.post'),
+    TitleSelector = ('.post-title'),
+    TitleListSelector = ('.titles');
 
   /* remove contents of titleList */
   const titleList = document.querySelector(TitleListSelector);
@@ -26,9 +27,8 @@ function generateTitleLinks() {
   titleList.innerHTML = html;
   console.log(html);
 }
-    generateTitleLinks();
+generateTitleLinks();
 
-{'use strict';
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
@@ -66,4 +66,38 @@ function generateTitleLinks() {
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+    function generateTags(){
+    const ArticleTagsSelector = ('.post-tags .list');
+    /* find all articles */
+    const articles = document.querySelectorAll(ArticleTagsSelector);
+    /* START LOOP: for every article: */
+    for (let article of articles){
+      /* find tags wrapper */
+      const tagWraper = article.querySelectorAll(ArticleTagsSelector);
+      /* make html variable with empty string */
+      let html = '';
+      /* get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+      console.log(articleTags);
+      /* split tags into array */
+      const articleTagsArray = articleTags.split(' ');
+      console.log(articleTagsArray);
+      /* START LOOP: for each tag */
+      for(let tag of articleTagsArray){
+        /* generate HTML of the link */
+        const linkHTML = '<li><a href="#' + tag + '">';
+        console.log(linkHTML);
+        /* add generated code to html variable */
+        articleTag.innerHTML += linkHTML;
+        /* insert link into html variable */
+        html += linkHTML;
+      /* END LOOP: for each tag */
+      }
+      /* insert HTML of all the links into the tags wrapper */
+      
+    /* END LOOP: for every article: */
+    
+  }
+}
+  generateTags();
 }
